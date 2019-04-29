0887794af7fb44bf67e174c234a65cf6800cca79
ianba <ianbabington93@gmail.com>
2019-04-29 15:10:16 +1200

    state before adding lifecylce hooks

--- project_2/react-complete-guide/src/componenets/Cockpit/Cockpit.js
+++ project_2/react-complete-guide/src/componenets/Cockpit/Cockpit.js
@@ -13,7 +13,7 @@ const cockpit = props => {
 
     return (
         <div className={classes.Cockpit}>
-          <h1>Hi, I'm a react app</h1>   
+          <h1>{props.title}</h1>   
           <p className={assignedClasses.join(' ')}>This is working</p>
           <button 
             className={btnClass}
--- project_2/react-complete-guide/src/containers/App.js
+++ project_2/react-complete-guide/src/containers/App.js
@@ -48,6 +48,7 @@ class App extends Component {
     return (
       <div className={classes.App}>
         <Cockpit
+          title={this.props.appTitle}
           click={this.togglePersonsHandler}
           length={this.state.persons.length}
           showPersons={this.state.showPersons}/>
