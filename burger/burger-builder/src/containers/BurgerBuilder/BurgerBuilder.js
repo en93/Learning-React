842e0d29b4ab77eda2be88f91c68b379941488d7
en93 <IanBabington93@gmail.com>
2019-05-31 09:05:21 +1200

    changes from this morning

--- burger/burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
+++ burger/burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import Aux from '../../hoc/Auxiliary';
 import Burger from '../../components/Burger/Burger';
+import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 
 class BurgerBuilder extends Component {
 
@@ -18,7 +19,7 @@ class BurgerBuilder extends Component {
         return (
             <Aux>
                 <Burger {...this.state}/>
-                <div>Build controls</div>
+                <BuildControls/>>
             </Aux>
         );
     }
